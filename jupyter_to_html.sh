6468ce9d6ae81aa073e7a73695b069f6fc3b9d86
hamsburger <harriszheng222@gmail.com>
2023-06-06 20:29:33 -0400

    trying out different rendering techniques now. Checkpoint

--- jupyter_to_html.sh
+++ jupyter_to_html.sh
@@ -1,4 +1,4 @@
-jupyter nbconvert --to html --no-input --execute ECE_Fourth_Year_Opinions.ipynb --TagRemovePreprocessor.enabled=True \
+jupyter nbconvert --to html --no-input --execute ./ECE_Fourth_Year_Opinions.ipynb --TagRemovePreprocessor.enabled=True \
         --TagRemovePreprocessor.remove_cell_tags hide_code \
         --template html_template
         
--- jupyter_to_html_any.sh
+++ jupyter_to_html_any.sh
@@ -1,4 +1,4 @@
-jupyter nbconvert --to html --no-input $1 --TagRemovePreprocessor.enabled=True \
+jupyter nbconvert --to html --no-input --execute $1 --TagRemovePreprocessor.enabled=True \
         --TagRemovePreprocessor.remove_cell_tags hide_code \
         --template html_template
         
